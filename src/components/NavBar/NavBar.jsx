051e323146348ee0c0e81e0bbfec6742cca2ae71
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2025-11-16 09:51:13 -0800

    add responsive hamburger menu for NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './NavBar.css';
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
-    <nav className='navbar'>
-      <div className='logo'>MyLogo</div>
-      <div className='nav-links'>
-        <a href='#hero'>Home</a>
-        <a href='#about'>About</a>
-        <a href='#projects'>Projects</a>
-        <a href='#contact'>Contact</a>
-      </div>
-    </nav>
+    <>
+      {/* Overlay (click to close) */}
+      {menuOpen && <div className='overlay' onClick={closeMenu}></div>}
+
+      <nav className='navbar'>
+        <div className='logo'>MyLogo</div>
+
+        {/* Menu links */}
+        <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <a href='#hero'>Home</a>
+          <a href='#about'>About</a>
+          <a href='#projects'>Projects</a>
+          <a href='#contact'>Contact</a>
+        </div>
+
+        {/* Animated hamburger */}
+        <div
+          className={`hamburger ${menuOpen ? 'open' : ''}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+    </>
   );
 };
 
